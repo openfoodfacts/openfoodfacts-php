0e87188e2126f480242350a6dc8ec369fd702a71
Dwarfex <h.oelze@wemake-it.de>
2020-03-06 11:57:00 +0100

    add tests for specific document and recursive sorting, add testing on different PHP versions

--- tests/OpenFoodFacts/CollectionTest.php
+++ tests/OpenFoodFacts/CollectionTest.php
@@ -4,22 +4,28 @@ use OpenFoodFacts\Collection;
 use OpenFoodFacts\Document;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class CollectionTest
+ */
 class CollectionTest extends TestCase
 {
     public function testConstructorMustPopulateListDocumentWithExistingDocuments()
     {
+
+        $documents = [
+            Document::createSpecificDocument('', []),
+            Document::createSpecificDocument('', []),
+        ];
+
         $collection = new Collection([
-            'products'=> $documents = [
-                Document::createSpecificDocument('', []),
-                Document::createSpecificDocument('', []),
-            ],
+            'products'=> $documents,
             'count' => 2,
             'page' => 1,
             'skip' => false,
             'page_size' => 2,
         ], '');
 
-        $this->assertCount(\count($documents), $collection);
+        $this->assertCount(count($documents), $collection);
         $docs = [];
         foreach($collection as $doc) {
             $docs[] = $doc;
--- tests/OpenFoodFacts/DocumentTest.php
+++ tests/OpenFoodFacts/DocumentTest.php
@@ -3,8 +3,12 @@
 use OpenFoodFacts\Document;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class DocumentTest
+ */
 class DocumentTest extends TestCase
 {
+
     public function testCreateSpecificDocumentMustCreatedADocumentFromEmptyIdentifier()
     {
         $this->assertInstanceOf(Document::class, Document::createSpecificDocument('', []));
@@ -13,6 +17,19 @@ class DocumentTest extends TestCase
     public function testCreateSpecificDocumentMustCreatedADocumentFromUnknownIdentifier()
     {
         $this->assertInstanceOf(Document::class, Document::createSpecificDocument('unknown', []));
+    }
+
+    public function testCreateSpecificDocumentFromKnownIdentifiers()
+    {
+        $types = [
+            'food'    => Document\FoodDocument::class,
+            'beauty'  => Document\BeautyDocument::class,
+            'pet'     => Document\PetDocument::class,
+            'product' => Document\ProductDocument::class,
+        ];
+        foreach ($types as $type => $className) {
+            $this->assertInstanceOf($className, Document::createSpecificDocument($type, []));
+        }
     }
 
     public function testGetDataMustReturnTheOriginalEmptyArray()
@@ -24,41 +41,94 @@ class DocumentTest extends TestCase
 
     public function testGetDataMustReturnTheNumericKeysArraySorted()
     {
-        $doc = Document::createSpecificDocument('', [
-            1 => 'b',
-            2 => 'c',
-            0 => 'a',
-        ]);
+        $doc = Document::createSpecificDocument(
+            '',
+            [
+                1 => 'b',
+                2 => 'c',
+                0 => 'a',
+            ]
+        );
 
-        $this->assertSame([
-            0 =>  'a',
-            1 =>  'b',
-            2 =>  'c',
-        ], $doc->getData());
+        $this->assertSame(
+            [
+                0 => 'a',
+                1 => 'b',
+                2 => 'c',
+            ],
+            $doc->getData()
+        );
     }
 
     public function testGetDataMustReturnTheStringKeysArraySorted()
     {
-        $doc = Document::createSpecificDocument('', [
-            'b' => 1,
-            'c' => 2,
-            'a' => 0,
-        ]);
+        $doc = Document::createSpecificDocument(
+            '',
+            [
+                'b' => 1,
+                'c' => 2,
+                'a' => 0,
+            ]
+        );
 
-        $this->assertSame([
-            'a' => 0,
-            'b' => 1,
-            'c' => 2,
-        ], $doc->getData());
+        $this->assertSame(
+            [
+                'a' => 0,
+                'b' => 1,
+                'c' => 2,
+            ],
+            $doc->getData()
+        );
+    }
+
+    public function testGetDataMustReturnMultilayerArraySorted(){
+        $doc = Document::createSpecificDocument(
+            '',
+            [
+                'b' => 1,
+                'c' => 2,
+                'a' => 0,
+                'd' => [
+                    'b' => 1,
+                    'c' => 2,
+                    'a' => [
+                        2 => 'a',
+                        1 => 'xyz',
+                        0 => 'c',
+                    ],
+                ],
+            ]
+        );
+
+        $expectedArray =
+            [
+                'a' => 0,
+                'b' => 1,
+                'c' => 2,
+                'd' => [
+                    'a' => [
+                        0 => 'c',
+                        1 => 'xyz',
+                        2 => 'a',
+                    ],
+                    'b' => 1,
+                    'c' => 2,
+                ],
+            ];
+
+        $this->assertSame($expectedArray, $doc->getData());
     }
 
     public function testGetDataMustReturnTheSameArray()
     {
-        $doc = Document::createSpecificDocument('', $data = [
-            0 => 0,
-            1 => 1,
-            2 => 2,
-        ]);
+        $doc = Document::createSpecificDocument(
+            '',
+            $data = [
+                0 => 0,
+                1 => 1,
+                2 => 2,
+            ]
+        );
 
         $this->assertSame($data, $doc->getData());
     }
